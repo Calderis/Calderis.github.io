8fcb4dc2ff9af59391c1e770ffd8a1790923bf79
Calderis <r.wetteren@gmail.com>
2017-11-07 21:47:45 +0100

    Set portfolio

---
+++ src/data.js
@@ -0,0 +1,177 @@
+module.exports = {
+    "socials": [
+        {
+            "icon": "twitter",
+            "url": "https://twitter.com/CalderisOne"
+        },
+        {
+            "icon": "github-circled",
+            "url": "https://github.com/Calderis"
+        },
+        {
+            "icon": "dribbble",
+            "url": ""
+        },
+        {
+            "icon": "skype",
+            "url": ""
+        },
+        {
+            "icon": "mail-alt",
+            "url": "mailto: email"
+        },
+        {
+            "icon": "linkedin-squared",
+            "url": "https://www.linkedin.com/in/wetterenremi/"
+        },
+        {
+            "icon": "icon-mail-alt",
+            "url": "Write me"
+        }
+    ],
+    "experiences": [
+        {
+            "__key": "7843adfd-76b0-4beb-972d-be23e6bb2e07",
+            "title": "Fullstack Developper at Hardware Club (second time)",
+            "timeline": "June 2017 - Present",
+            "description": "Work on the community platform and also work on increasing team efficiency !",
+            "responsibilities": [
+                "Worked on the backend side made out of Symfony 2",
+                "Worked on the chat based on Node.js & Socket.io",
+                "Also workedon the front-end build with Angular 2",
+                "I was in charge of analytics",
+                "Found ideas to improve the workflow",
+                "Made a lot of refactoring",
+                "Code Review"
+            ]
+        },
+        {
+            "__key": "7128635b-c6e0-4957-88e5-fa7166dfff29",
+            "title": "Fullstack Developper at Hardware Club",
+            "timeline": "June to October 2016",
+            "responsibilities": [
+                "Setup environment (back and front) with Docker, Symfony 2 and Angular 2",
+                "Create first functionnalities"
+            ],
+            "description": "Work with the lead on creating a very new platform to support the Hardware Club Community !"
+        },
+        {
+            "__key": "c9942151-41bd-49fe-bb73-37bb32e646c3",
+            "title": "Fullstack Developper at User Studio",
+            "timeline": "June to September 2015",
+            "description": "Work on internals tools such as search engine or team management system",
+            "responsibilities": [
+                "Develop solutions",
+                "Design products"
+            ]
+        }
+    ],
+    "name": "Rémi Wetteren",
+    "header": "Hi, I'm Rémi Wetteren",
+    "cta": {
+        "label": "Get my resume",
+        "url": "https://github.com/Calderis/portfolio/raw/master/Documents/cv_remi_wetteren_2017.pdf"
+    },
+    "testimonials": [],
+    "title": "Rémi Wetteren",
+    "projects": [
+        {
+            "__key": "7a138420-a1d3-47cc-82eb-cdabe778d65f",
+            "name": "Coloris",
+            "description": "Coloris is a software to calibrate your print. Analyse light, ICC Profile and color !",
+            "tags": [
+                "Angular2",
+                "Electron",
+                "ArgyllCMS",
+                "NodeJs"
+            ],
+            "alt": "Coloris Software preview",
+            "img": "http://www.coloris-app.fr/wp-content/uploads/2017/03/GamutViewer.jpg",
+            "url": "http://www.coloris-app.fr/"
+        },
+        {
+            "__key": "4c1a6f87-6cdc-4b24-9a12-90bdaacab08a",
+            "name": "PSA - FLASH Autonomous Medical Drone",
+            "description": "A R&D project with the groupe PSA around automous vehicules.",
+            "tags": [
+                "UX",
+                "UI",
+                "R&D"
+            ],
+            "img": "merc-01.png",
+            "url": "",
+            "alt": "PSA Project Overview"
+        },
+        {
+            "__key": "412d059a-cf0d-4e27-bc95-0414119e0c28",
+            "name": "Room",
+            "description": "Synchronized streaming service that allow you to watch whatever you want whenever you want with your friends !",
+            "tags": [
+                "Socket.io",
+                "Node.js",
+                "Electron",
+                "Torrent",
+                "IOS"
+            ],
+            "alt": "Coloris Software preview",
+            "img": "",
+            "url": "https://github.com/Calderis/Room-App-Desktop"
+        },
+        {
+            "__key": "9f178bda-6f7f-4379-a72f-e2ec649667c6",
+            "name": "MANA",
+            "description": "Stock market of companies ecological and social impact on the world.",
+            "tags": [
+                "Crawler",
+                "Algorithm",
+                "R&D",
+                "Electron"
+            ],
+            "alt": "No interface",
+            "img": "",
+            "url": "https://github.com/Calderis/MANADesktop"
+        },
+        {
+            "__key": "566b3e0a-1ab9-4b8c-9206-06403f434a98",
+            "name": "Sonder",
+            "description": "Sonder is a software that help you find startups that will succeed with their projects",
+            "tags": [
+                "Angular2",
+                "Electron",
+                "NodeJs"
+            ],
+            "alt": "Sonder Software preview",
+            "img": "http://www.coloris-app.fr/wp-content/uploads/2017/03/GamutViewer.jpg",
+            "url": "https://github.com/Calderis/hwc-business-tool"
+        },
+        {
+            "__key": "89f9f410-867a-48ae-b13d-90cabc664737",
+            "name": "Spwn",
+            "description": "Spwn is a software but also a framework made to build every thing very quickly (any language supported)",
+            "tags": [
+                "Angular2",
+                "Electron",
+                "ArgyllCMS",
+                "NodeJs"
+            ],
+            "alt": "Spwn Software preview",
+            "img": "",
+            "url": "https://github.com/Calderis/Spwn"
+        },
+        {
+            "__key": "d51f7d81-cbaf-4ef1-b875-fd9d280ece3b",
+            "name": "YummyTime",
+            "description": "Kind of spotify for food - choose your playlist and start cooking.",
+            "tags": [
+                "Ruby",
+                "IOS"
+            ],
+            "alt": "YummyTime Software preview",
+            "img": "",
+            "url": "https://github.com/Calderis/YummyTime-ruby"
+        }
+    ],
+    "email": "remi.wetteren@hetic.net",
+    "description": "I'm a web student and I look forward new experiences through multiple  projects. I'm crazy about every thing related to development, design and team work! Let's make something great together !",
+    "footerTitle": "Fullstack Developper"
+}
